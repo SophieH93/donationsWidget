0641e7f4456a6311bfe0f8cb427c64c8085af0cf
Sophie <sophiehickey123@gmail.com>
2022-01-16 12:23:53 +0000

    added data to mainCard & created Thermometer comp

--- donations-widget/src/MainCard.js
+++ donations-widget/src/MainCard.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Thermometer from "./Thermometer";
+
+const mockedData = {
+  id: 1,
+  goal: 5000,
+  raised: 1200,
+  logo: "https://previews.123rf.com/images/haris99/haris991807/haris99180701007/105539035-ilustraci%C3%B3n-de-vector-de-plantilla-de-dise%C3%B1o-de-logotipo-de-caridad.jpg",
+  title: "Fight Global Warming",
+};
 
 function MainCard(props) {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setDate] = useState(mockedData);
 
   return (
     <div className="card">
       <div className="card-header">
-        <div className="logo">Logo Goes Here!</div>
+        <div className="logo">
+          <img src={data.logo} alt="" width="100%" />
+        </div>
       </div>
       <div className="card-body">
-        <h4 className="title">Our data name</h4>
-        <p>Our thermometer</p>
+        <h4 className="title">{data.title}</h4>
+        <Thermometer goal={data.goal} name={data.name} raised={data.raised} />
       </div>
     </div>
   );
---
+++ donations-widget/src/Thermometer.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Thermometer = () => {
+  return <div></div>;
+};
+
+export default Thermometer;
