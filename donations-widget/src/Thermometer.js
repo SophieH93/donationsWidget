2ddb66aa59bda16fbf7fd39b5350b531b923dbf0
Sophie <sophiehickey123@gmail.com>
2022-01-16 19:04:07 +0000

    added funtionality to thermometer.js

--- donations-widget/src/Thermometer.js
+++ donations-widget/src/Thermometer.js
@@ -1,7 +1,119 @@
 import React, { useState } from "react";
 
-const Thermometer = () => {
-  return <div></div>;
+const Thermometer = (props) => {
+  const [goal, setGoal] = useState(props.goal);
+  const [raised, setRaised] = useState(props.raised);
+  const [value, setValue] = useState(1);
+  const [modal, setModal] = useState(false);
+
+  const getProgressBarWidth = (amountRaised, goal) => {
+    if ((amountRaised / goal) * 100 <= 100) {
+      return ((amountRaised / goal) * 100).toFixed(1);
+    } else {
+      return 100;
+    }
+  };
+
+  const handleModal = () => {
+    if (!modal) {
+      setModal(true);
+    } else {
+      setModal(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setValue(parseInt(e.target.value));
+  };
+
+  const handleDonation = () => {
+    setRaised(raised + value);
+    handleModal();
+  };
+
+  return (
+    <div>
+      <>
+        <div className="thermometer">
+          <div className="thermometer-raised">€ {raised.toFixed(2)}</div>
+          <div className="progress">
+            <div className="progress-labels">
+              <div>0%</div>
+              <div>Raised</div>
+              <div>100%</div>
+            </div>
+            <div className="progress-bar">
+              <div
+                className="progress-inner"
+                style={{ width: `${getProgressBarWidth(raised, goal)}%` }}
+              ></div>
+            </div>
+          </div>
+        </div>
+        <form className="donation-group">
+          <label>
+            <input
+              type="radio"
+              checked={value === 1}
+              onChange={handleChange}
+              value={1}
+            />
+            €1
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={value === 5}
+              onChange={handleChange}
+              value={5}
+            />
+            €5
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={value === 20}
+              onChange={handleChange}
+              value={20}
+            />
+            €20
+          </label>
+        </form>
+        <form className="donation-group">
+          <label>
+            <input
+              type="radio"
+              checked={value === 50}
+              onChange={handleChange}
+              value={50}
+            />
+            €50
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={value === 100}
+              onChange={handleChange}
+              value={100}
+            />
+            €100
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={value === 500}
+              onChange={handleChange}
+              value={500}
+            />
+            €500
+          </label>
+        </form>
+        <button className="give-btn" onClick={() => handleDonation()}>
+          PLEASE GIVE
+        </button>
+      </>
+    </div>
+  );
 };
 
 export default Thermometer;
