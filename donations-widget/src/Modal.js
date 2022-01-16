85b47141e2c3ecefbd82128788bdd2d6a2011148
Sophie <sophiehickey123@gmail.com>
2022-01-16 19:28:16 +0000

    Modal comp completed

--- donations-widget/src/Modal.js
+++ donations-widget/src/Modal.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-const Modal = () => {
-  return <div></div>;
+const Modal = (props) => {
+  return (
+    <div className="modal" data-status={props.status}>
+      <div className="modal-card">
+        <div className="modal-header">
+          <h2 className="title">{props.data.title}</h2>
+        </div>
+        <div className="modal-body">
+          <div className="subtitle">
+            We would like to thank you for your donation of{" "}
+          </div>
+          <span className="donation">€{props.value.toFixed(2)}</span>
+          <div className="subtitle">With your help we have raised </div>
+          <span className="donation-thin">€{props.raised} of our </span>
+          <span className="donation-thin">€{props.data.goal}</span> goal.{" "}
+        </div>
+        <button className="modal-btn" onClick={props.onClick}>
+          Donate some more!
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
